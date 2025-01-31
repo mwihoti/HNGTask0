71192aec365e74668cf14ffce9e29440774bcda3
mwihoti <danielmwihoti@gmail.com>
2025-01-31 04:23:42 +0300

    Add basic Express server with CORS enabled and root endpoint returning email, current datetime, and GitHub URL

--- app.js
+++ app.js
@@ -9,9 +9,20 @@ app.use(cors());
 
 // Define the api endpoint
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     const email = "danielmwihoti@gmail.com";
-    const currentDatetime = new Date().toISOString(); // To ISO format
-    
+    const current_datetime = new Date().toISOString(); // To ISO format
+    const github_url = "https://github.com/mwihoti/HNGTask0";
 
+    const response = {
+        email,
+        current_datetime,
+        github_url
+    };
+    res.status(200).json(response);
+
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 })
